db01591f3386674ac8521aec68dc7003771a7cc3
Larissa <larissa.mikkers@hotmail.com>
2024-12-09 09:18:20 +0100

    nav icons fixed + menu hover

--- my-app/src/components/navbar.jsx
+++ my-app/src/components/navbar.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 
-import homeIcon from '../assets/homeIcon.png';
-import loginIcon from '../assets/loginIcon.png';
+import homeIcon from '../assets/homeIcon.svg';
+import loginIcon from '../assets/loginIcon.svg';
+import dashboardIcon from '../assets/dashboardIcon.svg';
 
 const Navbar=()=>{
     return (  
         <nav> 
             <ul>
-                <li><Link to="/home" aria-label="Ga naar de homepagina" activeclassname="active"><img alt="home icon" src={homeIcon}/>Home</Link></li>
-                <li><Link to="/login" aria-label="Ga naar de login pagina" activeclassname="active"><img alt="login icon" src={loginIcon}/>Login</Link></li>
-                <li><Link to="/dashboard" aria-label="Ga naar de dashboard pagina" activeclassname="active">Dashboard</Link></li>
+                <li><NavLink to="/home" aria-label="Ga naar de homepagina" activeclassname="active"><img alt="home icon" src={homeIcon}/><span className="nav-text">Home</span></NavLink></li>
+                <li><NavLink to="/login" aria-label="Ga naar de login pagina" activeclassname="active"><img alt="login icon" src={loginIcon}/><span className="nav-text">Login</span></NavLink></li>
+                <li><NavLink to="/dashboard" aria-label="Ga naar de dashboard pagina" activeclassname="active"><img alt="dashboard icon" src={dashboardIcon}/><span className="nav-text">Dashboard</span></NavLink></li>
             </ul>
         </nav>
     )
